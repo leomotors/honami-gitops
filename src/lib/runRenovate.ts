c97d84faa2ff447402cc700e5b63528d4abd6b14
Leomotors Bot (real) <59821765+Leomotors@users.noreply.github.com>
2023-12-30 00:19:08 +0700

    prevent multiple renovate

--- src/lib/runRenovate.ts
+++ src/lib/runRenovate.ts
@@ -2,15 +2,26 @@ import { sendMessage } from "./discord.js";
 import { exec } from "./exec.js";
 import { log } from "./logger.js";
 
+let renovateRunning = false;
+
 export async function runRenovate() {
+  if (renovateRunning) {
+    log.info("Renovate already running, skipping...");
+    return;
+  }
+
+  renovateRunning = true;
+
   log.info("Running Renovate...");
   try {
     const { stderr, stdout } = await exec("pnpm run renovate");
-    console.log(stdout);
-    stderr && console.error(stderr);
+    log.normal(stdout);
+    stderr && log.error(stderr);
   } catch (err) {
     log.error("Renovate failed!");
     log.normal(`${err}`);
-    await sendMessage("# RENOVATE: Run failed");
+    await sendMessage(`# RENOVATE: Run failed\n${err}`.slice(0, 2000));
+  } finally {
+    renovateRunning = false;
   }
 }
