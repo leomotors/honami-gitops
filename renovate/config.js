f074ba7b6cb679a8e567af98a8a21f6f400ebaf5
Leomotors Bot (real) <59821765+Leomotors@users.noreply.github.com>
2023-12-31 16:53:54 +0700

    fix: ghcr.io auth

--- renovate/config.js
+++ renovate/config.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-undef */
 module.exports = {
   platform: "github",
   onboardingConfig: {
     extends: ["config:recommended"],
   },
+  hostRules: [
+    {
+      matchHost: "ghcr.io",
+      username: "leomotors-bot",
+      password: process.env.RENOVATE_TOKEN,
+    },
+  ],
 };
--- renovate/run.sh
+++ renovate/run.sh
@@ -4,6 +4,7 @@ sudo docker run \
     --rm \
     -e RENOVATE_REPOSITORIES="$RENOVATE_REPOSITORIES" \
     -e RENOVATE_GIT_AUTHOR="renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>" \
+    -e RENOVATE_TOKEN="$RENOVATE_TOKEN" \
     -v "./renovate/config.js:/usr/src/app/config.js" \
     renovate/renovate \
     --token "$RENOVATE_TOKEN" \
