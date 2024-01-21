e1e1c363e320198413b98dd6056cc884f78885fe
Leomotors Bot (real) <59821765+Leomotors@users.noreply.github.com>
2024-01-21 20:01:12 +0700

    fix: ayo wtf

--- src/lib/restart.ts
+++ src/lib/restart.ts
@@ -1,11 +1,18 @@
+import { sendMessage } from "./discord.js";
 import { exec } from "./exec.js";
 
 export async function restart(path: string, files: string[]) {
   for (const file of files) {
+    const start = performance.now();
+
     const targetPath = path + "/" + file.replace("/docker-compose.yml", "");
 
     await exec(
-      `cd ${targetPath} && sudo docker compose pull && sudo docker compose down && sudo docker compose up`,
+      `cd ${targetPath} && sudo docker compose pull && sudo docker compose down && sudo docker compose up -d`,
+    );
+
+    await sendMessage(
+      `:white_check_mark: Restarted ${file} in ${performance.now() - start}ms`,
     );
   }
 }
